9962ddfd4f365d111732cc38a6ef62bf37c6f57e
Gustavo Amaral <56303449+GussCloud@users.noreply.github.com>
2023-11-17 11:22:13 -0300

    Update
    Bloqeio em nova sala

---
+++ tilesets/Icone Evup.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Icone Evup" tilewidth="32" tileheight="32" tilecount="64" columns="8">
+ <image source="../../../../OneDrive/Evup/Imagens EVUP/icone_evup_braco.png" width="281" height="281"/>
+</tileset>
