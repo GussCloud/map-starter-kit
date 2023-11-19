b9cb8ab33acd27427875c34b8ac6b44774072f04
Gustavo Amaral <56303449+GussCloud@users.noreply.github.com>
2023-11-19 15:51:04 -0300

    correcoes
    Correcao na imagem do mapa

---
+++ tilesets/Logos_WVUP.tsx
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Logos_WVUP" tilewidth="3125" tileheight="2188" tilecount="7" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="3125" height="2188" source="../../../../Pictures/Imagens EVUP/icone_evup.png"/>
+ </tile>
+ <tile id="1">
+  <image width="153" height="181" source="../../../../Pictures/Imagens EVUP/icone_evup_braco (1).png"/>
+ </tile>
+ <tile id="2">
+  <image width="153" height="181" source="../../../../Pictures/Imagens EVUP/icone_evup_braco (2).png"/>
+ </tile>
+ <tile id="3">
+  <image width="281" height="281" source="../../../../Pictures/Imagens EVUP/icone_evup_braco.png"/>
+ </tile>
+ <tile id="4">
+  <image width="1250" height="797" source="../../../../Pictures/Imagens EVUP/logo_evup_azul (1).png"/>
+ </tile>
+ <tile id="5">
+  <image width="1031" height="219" source="../../../../Pictures/Imagens EVUP/logo_evup_azul.png"/>
+ </tile>
+ <tile id="6">
+  <image width="1031" height="219" source="../../../../Pictures/Imagens EVUP/logo_evup_braco.png"/>
+ </tile>
+</tileset>
